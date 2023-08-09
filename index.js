1b78e8c34fee7a8c3695dfe80bc29fdb3ab3cf0d
anthonygift <aduniesohe347@gmail.com>
2023-08-10 00:55:06 +0100

    class work

---
+++ index.js
@@ -0,0 +1,85 @@
+// Booleans:
+// let x = true;
+/*let y = true;
+let z = false; 
+(x==y) //returns true
+(x==z) //return false*/
+
+// strings:single quotes
+
+//let a = 3
+// b = 1
+//let c =a + b
+
+//console.log (a + "+" + b + "=" + c)
+
+//let firstName = "Gift"
+//let age = "20"
+//let profile = firstName + age
+
+//console.log ("result =" + " " + firstName + " " + "is" + " " + age + "years old")
+//console.log ("my sister" + " " + firstName + " " + "will be" +  " " + age+ " " + "next year")
+//console.log ("apple =" + " " + firstName + " " + "has" + " " + age + " " + "apple")
+//console.log (firstName + " " + age + " " + "key =" +  " " + "happiness")
+
+
+//let school = [
+   // {
+    //name: "Eagle mountain colleage"
+    //location: "lagos nigeria"
+   // uniform: "blue and white"
+   // school number:"144"
+    //}
+   // {
+   // name: "Granite Bay School"
+    //location: "Toronto canada"
+   // uniform: "grey and white"
+   // school number:"155"
+   // }
+   // {
+       // name: "Somerset Technical school"
+       // location:"Scotland united kingdom"
+       // uniform : "black and white"
+       // school number:"175"
+   // }
+//]
+
+//let car = { 
+    //Brand:"toyota corolla"
+    //car No : 40
+    //colour : "Blue"
+    //model : "corolla 2010"
+    //plate number: SH225
+
+//}
+
+
+//Class Work
+let noiseMaker = [
+    {
+       name: "Gabriel",
+     surname: "Solomon",
+        skinColor: "black",
+        matricNumber: "123",
+        size: {
+           height: "20cm",
+           weight: "30"
+       }
+    },
+    {
+        name: "Taiwo",
+       surname: "Adedokun",
+       skinColor: "black",
+        matricNumber: "124"
+    }
+]
+
+console.log (noiseMaker[0].surname)
+noiseMaker[1].name="kehinde"
+console.log (noiseMaker[1])
+console.log (noiseMaker[0].size.height)
+
+noiseMaker[1].transmission= "age"
+console.log (noiseMaker[1].transmission)
+
+
